23355d5e650bf3877e8152959bacc90ec8ea6052
Arnold Daniels <arnold@jasny.net>
2016-12-02 18:53:15 +0100

    Ignore before()/after() for coverage

--- src/Controller/RouteAction.php
+++ src/Controller/RouteAction.php
@@ -77,6 +77,7 @@ trait RouteAction
     
     /**
      * Called before executing the action.
+     * @codeCoverageIgnore
      * 
      * <code>
      * protected function beforeAction()
@@ -95,6 +96,7 @@ trait RouteAction
     
     /**
      * Called before executing the action.
+     * @codeCoverageIgnore
      */
     protected function after()
     {
