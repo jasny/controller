be280e9c13425ff6a464f8d6a568a7803cdfdf63
arnold <arnold@jasny.net>
2022-07-30 22:22:31 +0200

    Test for BodyParam

---
+++ tests/Parameter/BodyParamTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Jasny\Test\Controller\Parameter;
+
+use Jasny\Controller\Parameter\BodyParam;
+use Jasny\Controller\ParameterException;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * @covers \Jasny\Controller\Parameter\BodyParam
+ * @covers \Jasny\Controller\Parameter\SingleParameter
+ */
+class BodyParamTest extends TestCase
+{
+    public function provider()
+    {
+        return [
+            ['foo', 'int', 'bar', 'string', 'foo', 42],
+            ['foo', null, 'bar', 'string', 'foo', '42'],
+            [null, 'int', 'bar', 'string', 'bar', 42],
+            [null, null, 'bar', 'string', 'bar', '42'],
+        ];
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function test($consKey, $consType, $name, $type, $attribute, $expected)
+    {
+        $parameter = new BodyParam($consKey, $consType);
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request->expects($this->once())->method('getParsedBody')
+            ->willReturn([$attribute => '42', 'ot' => 10]);
+
+        $value = $parameter->getValue($request, $name, $type);
+        $this->assertEquals($expected, $value);
+    }
+
+    public function testMissingOptional()
+    {
+        $parameter = new BodyParam();
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request->expects($this->once())->method('getParsedBody')
+            ->willReturn(['ot' => 10]);
+
+        $this->assertNull(
+            $parameter->getValue($request, 'foo', null, false)
+        );
+    }
+
+    public function testMissingRequired()
+    {
+        $parameter = new BodyParam();
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request->expects($this->once())->method('getParsedBody')
+            ->willReturn(['ot' => 10]);
+
+        $this->expectException(ParameterException::class);
+        $this->expectExceptionMessage("Missing required body parameter 'foo'");
+
+        $parameter->getValue($request, 'foo', null, true);
+    }
+
+    public function testInvalidTypeInConstructor()
+    {
+        $this->expectException(\DomainException::class);
+        $this->expectExceptionMessage("Undefined parameter type 'big'");
+
+        new BodyParam('foo', 'big');
+    }
+}
